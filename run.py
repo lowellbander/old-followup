97c57e154fe13a266631ce8188faf41bd2d6decf
Lowell Bander <lowellbander@gmail.com>
2014-08-16 21:33:44 -0700

    working on submission

--- run.py
+++ run.py
@@ -14,8 +14,30 @@ def get(famID):
 
 @app.route('/<fid>', methods=['GET','POST'])
 def index(fid):
-    if request.method == 'POST':
-        return "Submitted!"
+    #process submission
+    first = request.args.get('first')
+    second = request.args.get('second')
+    third = request.args.get('third')
+    if bool(first) or bool(second) or bool(third):
+        review = ''
+        if bool(first):
+            review += "How awesome was your tour guide?\n\n"
+            review += first + '\n\n'
+        if bool(second):
+            review += "Was there anything you would have liked to have heard about, but didn't?\n\n"
+            review += second + '\n\n'
+        if bool(third):
+            review += "Was there anything you could have done without?\n\n"
+            review += third
+        print review
+
+        params = {'review': review}
+        response = unirest.put(os.environ['CLERK_URL'], params=params)
+        print response.body
+
+        return "Submitted"
+
+    #serve form
     try:
         data = {'id': fid}
         data['name'] = get(data['id'])['name']
