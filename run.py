74a511eb6b12cc4ec620102afa79cf5e01424b29
Lowell Bander <lowellbander@gmail.com>
2014-08-16 20:29:52 -0700

    bug fixes; uses env var

--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request
+import os
 import unirest
-import settings
 
 app = Flask(__name__)
 app.debug = True
 
 def get(famID):
-    response = unirest.get(settings.API_URL + 'tour/' + famID)
-    return response.body
+    try:
+        response = unirest.get(os.environ['CLERK_URL'] + 'tour/' + famID)
+        return response.body
+    except TypeError:
+        return None
 
 @app.route('/')
 def index():
-    data = {'id': request.args.get('id')}
-    data['name'] = get(data['id'])['name']
-    return render_template('index.html', data=data)
+    try:
+        data = {'id': request.args.get('id')}
+        data['name'] = get(data['id'])['name']
+        return render_template('index.html', data=data)
+    except TypeError:
+        return "Error. Probably no ID in URL."
 
 if __name__ == '__main__':
     app.run()
