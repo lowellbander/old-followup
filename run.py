b9dcd34b1a26cf3e30f74e6061e904317ee71660
Lowell Bander <lowellbander@gmail.com>
2014-08-16 20:02:20 -0700

    bug fixes

--- run.py
+++ run.py
@@ -6,14 +6,20 @@ app = Flask(__name__)
 app.debug = True
 
 def get(famID):
-    response = unirest.get(settings.API_URL + 'tour/' + famID)
-    return response.body
+    if famID is not None:
+        response = unirest.get(settings.API_URL + 'tour/' + famID)
+        return response.body
+    else:
+        return None
 
 @app.route('/')
 def index():
     data = {'id': request.args.get('id')}
-    data['name'] = get(data['id'])['name']
-    return render_template('index.html', data=data)
+    try:
+        data['name'] = get(data['id'])['name']
+        return render_template('index.html', data=data)
+    except TypeError:
+        return "No ID in URL."
 
 if __name__ == '__main__':
     app.run()
