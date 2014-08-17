6cfc972a86073814fb74c404ae8ce96c1d2c798e
Lowell Bander <lowellbander@gmail.com>
2014-08-16 20:32:11 -0700

    left in <<<<< HEAD nonsense

--- run.py
+++ run.py
@@ -6,36 +6,20 @@ app = Flask(__name__)
 app.debug = True
 
 def get(famID):
-<<<<<<< HEAD
     try:
         response = unirest.get(os.environ['CLERK_URL'] + 'tour/' + famID)
         return response.body
     except TypeError:
-=======
-    if famID is not None:
-        response = unirest.get(settings.API_URL + 'tour/' + famID)
-        return response.body
-    else:
->>>>>>> FETCH_HEAD
         return None
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     try:
         data = {'id': request.args.get('id')}
         data['name'] = get(data['id'])['name']
         return render_template('index.html', data=data)
     except TypeError:
         return "Error. Probably no ID in URL."
-=======
-    data = {'id': request.args.get('id')}
-    try:
-        data['name'] = get(data['id'])['name']
-        return render_template('index.html', data=data)
-    except TypeError:
-        return "No ID in URL."
->>>>>>> FETCH_HEAD
 
 if __name__ == '__main__':
     app.run()
