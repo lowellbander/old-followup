0c5125dd5aad5d2b857ef1051f331d55c137c255
Lowell Bander <lowellbander@gmail.com>
2014-08-16 18:04:49 -0700

    gets family name from URL

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import unirest
+import settings
 
 app = Flask(__name__)
 app.debug = True
 
+def get(famID):
+    response = unirest.get(settings.API_URL + 'tour/' + famID)
+    return response.body
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = {'id': request.args.get('id')}
+    data['name'] = get(data['id'])['name']
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
